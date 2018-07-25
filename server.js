ccdef385b8bbed6ea6781c4bd6ab684b729bed88
mehedi hasan limon <mhlimon860@gmail.com>
2018-07-25 04:55:25 +0000

    classwork8

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ var http = require('http');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var mongo =require("mongodb");
+var db,uri="mongodb://localhost:27017";
+//var db,uri="mongodb://"+ process.env.IP+"28017";
+mongo.MongoClient.connect(uri,
+{useNewUrlParser:true},
+function(err,client){
+  if(err){
+    console.log('could not connect to mongodb');
+  }else{
+    db=client.db('node-cw8');
+  }
+});
+  var save=function (email_data){
+    db.createCollection('users',function(err,collection){});
+    var collection=db.collection('users');
+    collection.save(email_data);
+  };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
@@ -18,7 +35,10 @@ app.get('/email',function(req,res){
 });
 app.post('/submit_user',function(req,res)
 {
+  
     console.log(req.body);
+    save(req.body);
+    res.state(200);
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
